83c55358b8d89ee89845c2db750e6a660fa08faa
Catalina Hernandez <cataxto@gmail.com>
2023-05-23 21:58:24 -0500

    Realizando el registro

--- Clase_19/scripts/signup.js
+++ Clase_19/scripts/signup.js
@@ -15,7 +15,17 @@ window.addEventListener('load', function () {
     /* -------------------------------------------------------------------------- */
     form.addEventListener('submit', function (event) {
         event.preventDefault();
+        console.log('Preparando datos');
+        //Validar los dos pwd sean iguales
+        const datosUsuario={
+            firstName: inputNombre.value,
+            lastName: inputApellido.value,
+            email: inputEmail.value,
+            password: inputPassword.value
+        }
 
+        console.log(datosUsuario);
+        realizarRegister(datosUsuario);
 
 
 
@@ -24,8 +34,32 @@ window.addEventListener('load', function () {
     /* -------------------------------------------------------------------------- */
     /*                    FUNCIÓN 2: Realizar el signup [POST]                    */
     /* -------------------------------------------------------------------------- */
-    function realizarRegister(settings) {
-        
+    function realizarRegister(datosUsuario) {
+        const url = 'https:ctd-todo-api.herokuapp.com/v1/users';
+
+        const config = {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(datosUsuario)
+        }
+
+        fetch(url, config).then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+
+                if (data.jwt) {
+                    localStorage.setItem('jwt', data.jwt);
+                    location.replace('mis-tareas.html');
+                }
+
+            }).catch((response) => {
+                console.error(response);
+            })
+
+
+
 
 
 
